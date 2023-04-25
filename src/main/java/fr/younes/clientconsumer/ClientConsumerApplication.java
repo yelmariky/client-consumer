c6e66958df48a3d975c53d3e2e85eab8353c6485
yelmariky <yelmariky@gmail.com>
2023-04-25 13:00:13 +0200

    dasactive or active consumer

--- src/main/java/fr/younes/clientconsumer/ClientConsumerApplication.java
+++ src/main/java/fr/younes/clientconsumer/ClientConsumerApplication.java
@@ -2,16 +2,12 @@ package fr.younes.clientconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import fr.younes.libkafkaspring.config.KafkaConfig;
-
-import  fr.younes.libkafkaspring.config.EnableMyKafkaConsumer;
 
 
 @SpringBootApplication
 @Import({KafkaConfig.class})
-@EnableMyKafkaConsumer
 public class ClientConsumerApplication {
 
     public static void main(String[] args) {
