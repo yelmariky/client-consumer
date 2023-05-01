30a62bb79a0bba1f9c83007d20dc3644ad623521
yelmariky <yelmariky@gmail.com>
2023-05-01 17:29:50 +0200

    implement sleuth

--- src/main/java/fr/younes/clientconsumer/CustomMessageProcessor.java
+++ src/main/java/fr/younes/clientconsumer/CustomMessageProcessor.java
@@ -1,14 +1,17 @@
 package fr.younes.clientconsumer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import fr.younes.libkafkaspring.processor.MessageProcessor;
 @Service
 public class CustomMessageProcessor implements MessageProcessor {
+    private static final Logger log = LoggerFactory.getLogger(CustomMessageProcessor.class);
 
     @Override
     public void process(String message) {
-        System.out.println("Processing message: " + message);
+        log.info("**Processing message: " + message);
         // Ajoutez ici votre logique de traitement
     }
 }
